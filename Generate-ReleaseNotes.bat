d5f7bb4383fa1a31f75f1515d6b2c876bc345bb4
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-09-28 17:43:26 +0200

    HotChocolate 12.0.0 (#7)

--- Generate-ReleaseNotes.bat
+++ Generate-ReleaseNotes.bat
@@ -1,7 +1,7 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
-SET version=0.6.0
+SET version=0.7.0
 
-GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid doc --version %version%
+GitHubReleaseNotes --output ReleaseNotes.md --skip-empty-releases --exclude-labels question invalid doc --version %version% --token %GH_TOKEN%
 
-GitHubReleaseNotes --output PackageReleaseNotes.txt --skip-empty-releases --exclude-labels question invalid doc --template PackageReleaseNotes.template --version %version%
+GitHubReleaseNotes --output PackageReleaseNotes.txt --skip-empty-releases --exclude-labels question invalid doc --template PackageReleaseNotes.template --version %version% --token %GH_TOKEN%
